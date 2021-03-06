2de88e31240cf3b0d3ad855d386a9eaa1782ee46
DANIELMEJÍA794 <novovacuum@gmail.com>
2021-03-06 12:49:47 -0500

    primer commit

---
+++ CSS/ArchivoJavaScript.js
@@ -0,0 +1,16 @@
+alert('Aquí estoy');
+var edadAdulta = 25;
+var menorEdad = 18;
+
+function verPrompt(){
+    //if{}()else{}   ---> Si es que se cumple (), entonces ejecuto {}, sino se cumple ejecuta else{};
+    if(edadAdulta<18){
+        prompt('entro');
+        console.log('mi primera función');
+    } else if (menorEdad == 18){
+        alert('es mayor de edad')
+    }else{
+        alert('No era cierto')
+    }
+}
+// var verAlerta = () => {}
