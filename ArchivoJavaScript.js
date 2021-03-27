f1d4224874e096e08014225104e2fb462cb5aa6e
DANIELMEJIA794 <80170013+DANIELMEJIA794@users.noreply.github.com>
2021-03-27 16:58:39 -0500

    Add files via upload

---
+++ ArchivoJavaScript.js
@@ -0,0 +1,66 @@
+var primeraPagina = document.getElementById('primera');
+var segundaPagina = document.getElementById('segunda');
+var terceraPagina = document.getElementById('tercera');
+var cuartaPagina = document.getElementById('cuarta');
+var Listautores = document.getElementById('Listautores');
+var LisCat = document.getElementById('LisCat');
+
+function irPrimera(){
+    primeraPagina.style.display = 'block';
+    segundaPagina.style.display = 'none';
+    terceraPagina.style.display = 'none';
+    cuartaPagina.style.display = 'none';}
+
+function irSegunda(){
+    primeraPagina.style.display = 'none';
+    segundaPagina.style.display = 'block';
+    terceraPagina.style.display = 'none';
+    cuartaPagina.style.display = 'none';
+}
+
+function irTercera(){
+    console.log('funcionas?')
+    primeraPagina.style.display = 'none';
+    segundaPagina.style.display = 'none';
+    terceraPagina.style.display = 'block';
+    cuartaPagina.style.display = 'none';
+}
+function irCuarta(){
+    primeraPagina.style.display = 'none';
+    segundaPagina.style.display = 'none';
+    terceraPagina.style.display = 'none';
+    cuartaPagina.style.display = 'block';
+}
+
+function mostrarListas(listas){
+    //operador ternario:    condicion ? primera sentencia : segunda sentencia;
+    if(listas.style.display === 'block'){
+        listas.style.display = 'none'} else{
+            listas.style.display = 'flex';
+            listas.style = 'flex-direction: columns';
+        }
+}
+
+
+
+
+
+
+
+
+// alert('Aquí estoy');
+// var edadAdulta = 25;
+// var menorEdad = 18;
+
+// function verPrompt(){
+//     //if{}()else{}   ---> Si es que se cumple (), entonces ejecuto {}, sino se cumple ejecuta else{};
+//     if(edadAdulta<18){
+//         prompt('entro');
+//         console.log('mi primera función');
+//     } else if (menorEdad == 18){
+//         alert('es mayor de edad')
+//     }else{
+//         alert('No era cierto')
+//     }
+// }
+// var verAlerta = (parametros) => { ejecución}
